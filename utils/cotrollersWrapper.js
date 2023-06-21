c6a1cfd6543bfd70b4b30cde5fa31911e22187e7
Яна <112972800+Yana-moon@users.noreply.github.com>
2023-06-21 11:01:49 +0300

    done hw02

---
+++ utils/cotrollersWrapper.js
@@ -0,0 +1,13 @@
+const controllersWrapper = (controllers) => {
+    const func = async (req, res, next) => {
+      try {
+        await controllers(req, res, next);
+      } catch (error) {
+        next(error);
+      }
+    };
+  
+    return func;
+  };
+  
+  module.exports = controllersWrapper;
---
+++ utils/index.js
@@ -0,0 +1,7 @@
+const AppError = require("./appError");
+const controllersWrapper = require("./controllersWrapper");
+
+module.exports = {
+  AppError,
+  controllersWrapper,
+};

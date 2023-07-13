41f797b8fa8b7ae76b82d4415aa3eec85e269ca5
Яна <112972800+Yana-moon@users.noreply.github.com>
2023-07-13 23:16:10 +0300

    done 5 hw

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,10 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
+const gravatar = require("gravatar");
+const path = require("path");
+const fs = require("fs/promises");
+const Jimp = require("jimp");
 
 const { User, schemas } = require("../models/user");
 
@@ -8,6 +12,8 @@ const { AppError, controllersWrapper } = require("../utils");
 
 dotenv.config();
 const { JWT_SECRET } = process.env;
+
+const avatarsDir = path.join(__dirname, "../", "public", "avatars");
 
 const register = async (req, res) => {
   const { error } = schemas.registerSchema.validate(req.body);
@@ -22,7 +28,9 @@ const register = async (req, res) => {
   }
 
   const hashPassword = await bcrypt.hash(password, 10);
-  const newUser = await User.create({ ...req.body, password: hashPassword });
+  const avatarURL = gravatar.url(email);
+
+  const newUser = await User.create({ ...req.body, password: hashPassword, avatarURL });
 
   res.status(201).json({
     email: newUser.email,
@@ -98,10 +106,27 @@ const updateSubscription = async (req, res) => {
   res.status(200).json({ email, subscription });
 };
 
+const updateAvatar = async (req, res) => {
+  const { _id } = req.user;
+  const { path: tempUpload, originalname } = req.file;
+  const filename = `${_id}_${originalname}`;
+  const resultUpload = path.join(avatarsDir, filename);
+
+  await Jimp.read(tempUpload).then((avatar) => {
+    return avatar.resize(250, 250).write(tempUpload);
+  });
+  await fs.rename(tempUpload, resultUpload);
+  const avatarURL = path.join("avatars", filename);
+  await User.findByIdAndUpdate(_id, { avatarURL });
+
+  res.status(200).json({ avatarURL });
+};
+
 module.exports = {
   register: controllersWrapper(register),
   login: controllersWrapper(login),
   getCurrent: controllersWrapper(getCurrent),
   logout: controllersWrapper(logout),
   updateSubscription: controllersWrapper(updateSubscription),
+  updateAvatar: controllersWrapper(updateAvatar),
 };
